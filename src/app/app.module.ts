e738d0a10ce2baaf829435e2f88a66ab38eb4dfe
Zubair Siddique <zubairsidd20@gmail.com>
2022-07-26 21:46:59 +0530

    login implemente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,7 +10,7 @@ import { PremiumComponent } from './premium/premium.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,13 +21,10 @@ import { ContactComponent } from './contact/contact.component';
     PremiumComponent,
     LoginComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from '../models/user/login';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  url: string = 'https://localhost:44397/api/user/login';
+  constructor(private http: HttpClient) {}
+
+  login(loginDetails: Login) {
+    return this.http.post(this.url, loginDetails);
+  }
+}
