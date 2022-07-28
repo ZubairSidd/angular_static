c99306b9d870634e5e427ef100df0e82116051d5
kaifgour <kaifi252000@gmail.com>
2022-07-28 12:13:12 +0530

    added buy insurance page

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,13 +14,4 @@ export class AuthService {
     return this.http.post(this.url, loginDetails);
   }
 
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
-
-  constructor() { }
-
 }
